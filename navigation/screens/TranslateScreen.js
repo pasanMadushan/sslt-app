b1eae28d31e9e11b0ada0ea240d3b7c16eee3e31
Pasan Madushan <pasanmadushan@gmail.com>
2023-01-14 10:45:47 +0530

    camera integration

--- navigation/screens/TranslateScreen.js
+++ navigation/screens/TranslateScreen.js
@@ -1,13 +1,62 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {View, Text, StyleSheet, Button} from 'react-native';
+import { Camera } from 'expo-camera';
 
 
 export default function TranslateScreen({navigation}){
+
+    const [hasCameraPermission, setHasCameraPermission] = useState(null);
+    const [camera, setCamera] = useState(null);
+    const [record, setRecord] = useState(null);
+    const [type, setType] = useState(Camera.Constants.Type.back);
+    const video = React.useRef(null);
+    const [status, setStatus] = React.useState({});
+
+    useEffect(()=>{
+       (async ()=>{
+        const cameraStatus = await Camera.requestCameraPermissionsAsync();
+        setHasCameraPermission(cameraStatus.status === 'granted');
+       })(); 
+    },[]);
+
+    const takeVideo = async () =>{
+        if(camera){
+            const data = await camera.recordAsync({
+                maxDuration:10
+            })
+            setRecord(data.uri);
+            console.log(data.uri);
+        }
+    }
+
+    const stopVideo = async ()=>{
+        camera.stopRecording();
+    }
+
+    if(hasCameraPermission === null ){
+        return <View />;
+    }
+
+    if(hasCameraPermission === false ){
+        return (<View style={styles.container}>
+            <Text>No Camera Access</Text>
+            </View>);
+    }
+
     return(
         <View style={styles.container}>
-            <Text>
-                This is Translate Screen
-            </Text>
+           
+                <Camera
+                    ref = { ref => setCamera(ref) }
+                    style = {styles.fixedRatio}
+                    type = {type}
+                    
+                />
+          
+
+            <Button title='Take Video' onPress={()=>takeVideo()}/>
+            <Button title='Stop Video' onPress={()=>stopVideo()}/>
+
         </View>
     );
 }
@@ -17,6 +66,9 @@ const styles = StyleSheet.create({
        flex:1,
        alignItems:'center',
        justifyContent:'center'
+    },
+    cameraContainer:{
+
     }
    })
    
